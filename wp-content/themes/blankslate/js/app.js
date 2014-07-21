bc0b2ff65d25a0a2c58aa473cc32fcb91546d692
unknown <Administrator@LAB706.tumo.lab>
2014-07-21 19:47:15 +0400

    asdasd

--- wp-content/themes/blankslate/js/app.js
+++ wp-content/themes/blankslate/js/app.js
@@ -1,40 +1,21 @@
 app = {}, UTIL = {
-    fire: function(t, n, e) {
-        var o = app;
-        n = void 0 === n ? "init" : n, "" !== t && o[t] && "function" == typeof o[t][n] && o[t][n](e);
+    fire: function(o, n, i) {
+        var c = app;
+        n = void 0 === n ? "init" : n, "" !== o && c[o] && "function" == typeof c[o][n] && c[o][n](i);
     },
     loadEvents: function() {
-        var t = document.body.id;
-        UTIL.fire("common"), $.each(document.body.className.split(/\s+/), function(n, e) {
-            UTIL.fire(t), UTIL.fire(t, e);
+        var o = document.body.id;
+        UTIL.fire("common"), UTIL.fire(o), $.each(document.body.className.split(/\s+/), function(n, i) {
+            UTIL.fire(o, i);
         }), UTIL.fire("common", "finalize");
     }
 }, $(document).ready(UTIL.loadEvents), app.common = {
-    init: function() {},
+    init: function() {
+        $(document).scroll(function() {
+            $(document).scrollTop() >= 300 ? $(".UpArrow").css("display", "block") : 300 > $(document).scrollTop() && $(".UpArrow").css("display", "none");
+        }), $(".UpArrow").click(function() {});
+    },
     finalize: function() {}
-}, jQuery.fn.fadeOutAndRemove = function(t) {
-    t = t !== void 0 ? t : 300, $(this).fadeOut(t, function() {
-        $(this).remove();
-    });
-}, jQuery.fn.itemSearch = function(t) {
-    var n = $(this).attr("id"), e = null;
-    $("#" + n + " .openmodal").click(function() {
-        $("#modal-" + n).modal(), $(t.containers.results).empty();
-    }), $("#modal-" + n + " button[name='search']").click(function() {
-        var o = {};
-        $.each($("#modal-" + n + " form").serializeArray(), function(t, n) {
-            o[n.name] = n.value;
-        }), $.post(t.url, o, function(n) {
-            e = n.data, $(t.containers.results).empty(), $.each(n.data, function(n, e) {
-                e.resultid = n;
-                var o = $(t.templates.result).html(), a = Mustache.render(o, e);
-                $(t.containers.results).append(a);
-            });
-        }, "json");
-    }), $(document).on("click", t.containers.results + " button.select", function() {
-        var n = $(this).attr("data-result-id"), o = e[n], a = $(t.templates.item).html(), i = Mustache.render(a, o);
-        $(t.containers.items).append(i), t.multiSelect === !1 && app.common.closeModal();
-    }), $(document).on("click", t.containers.items + " .trash", function() {
-        $(this).parent().parent().fadeOutAndRemove();
-    });
+}, app.home = {
+    init: function() {}
 };
